535125c8cb0a38c4684e6d020553312c49b96f17
jinwook <jin942002@naver.com>
2019-04-14 18:45:27 +0900

    챔피언들을 출력하는 기능 구현.
    관련함수: toString() in Champion class.

--- src/main/java/domain/Champion.java
+++ src/main/java/domain/Champion.java
@@ -1,8 +1,11 @@
 package domain;
 
+import javax.swing.*;
 import java.util.*;
 
 public class Champion {
+    static final String SEPERATOR = ",";
+    static final String ARE_WINNERS = "가 최종 우승했습니다.";
     List<Car> champions;
 
     public Champion(List<Car> cars) {
@@ -46,8 +49,16 @@ public class Champion {
         return positionList.get(positionList.size() - 1);
     }
 
+    @Override
+    public String toString() {
+        String championString = "";
+        int i;
+        for(i = 0; i < champions.size() - 1; i++) {
+            championString += champions.get(i).toString() + SEPERATOR;
+        }
 
+        championString += champions.get(i);
 
-
-
+        return championString + ARE_WINNERS;
+    }
 }
