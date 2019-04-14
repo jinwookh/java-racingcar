c70f8f677ada8bd4d5f5504efee7c6c07bc9c25d
jinwook <jin942002@naver.com>
2019-04-14 14:14:13 +0900

    문자열을 차 이름들로 변환하는 기능 구현.
    관련함수: convertInputToNames() in CarNames class.

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -3,7 +3,7 @@ import domain.CarNames;
 public class Solution {
     public static void main(String[] args) {
 
-        CarNames carNames = new CarNames("  \n");
-
+        CarNames carNames = new CarNames("car1,car2,,,car5");
+        System.out.println(carNames.toString());
     }
 }
--- src/main/java/domain/CarNames.java
+++ src/main/java/domain/CarNames.java
@@ -1,5 +1,6 @@
 package domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CarNames {
@@ -15,11 +16,22 @@ public class CarNames {
             throw new IllegalArgumentException("쉼표로 구분된 차 이름들을 입력해 주세요. " +
                     "차 이름은 영문 또는 숫자 혼합이어야 합니다.");
         }
-
+        this.names = convertInputToNames(input);
 
     }
 
-    int countValidNames(String input) {
+    private List<String> convertInputToNames(String input) {
+        List<String> names = new ArrayList<>();
+        for (String string : input.split(SEPARATOR)) {
+            if (checkValidity(string.trim())) {
+                names.add(string);
+            }
+        }
+
+        return names;
+    }
+
+    private int countValidNames(String input) {
         int count = 0;
         String[] namesBeforeChecked = input.split(SEPARATOR);
 
@@ -46,5 +58,10 @@ public class CarNames {
             return true;
         }
         return false;
+    }
+
+    @Override
+    public String toString() {
+        return names.toString();
     }
 }
